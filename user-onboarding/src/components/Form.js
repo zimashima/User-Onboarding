c330e281e74e9b1636f49687969a50dc91f5baa7
Mashima <52222646+zimashima@users.noreply.github.com>
2019-11-14 16:48:50 -0500

    added form validation using Yup, styled form using CSS and styled-components

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,7 +1,7 @@
-import React from "react"
-import styled from "styled-components"
-import {withFormik, Form, Field} from "formik"
-import { __values } from "tslib"
+import React from "react";
+import styled from "styled-components";
+import {withFormik, Form, Field} from "formik";
+import * as Yup from 'yup';
 
 const TheButton = styled.button`
     background-color: gray;
@@ -9,21 +9,45 @@ const TheButton = styled.button`
     padding: 10px;
     border: 1px solid gray;
     border-radius: 10px;
+    font-size: 20px;
 `
 
-function UserForm({values}) {
+const FormDiv = styled.div`
+    background-color: #696969;
+    color: white;
+    width: 500px;
+    margin: 0 auto;
+    border-radius: 30px;
+    font-size: 16px;
+`
+const Perror = styled.p`
+    font-weight: bold;
+`
+function UserForm({values, touched, errors}) {
     return (
-        <div>
-            <Form>
-                <Field type="text" name="usersname"/>
-                <Field type="text" name="email"/>
-                <Field type="text" name="password"/>
+        <FormDiv>
+            <Form className="mainForm">
+                <label>Name</label>
+                <Field type="text" name="usersname" className="inputForm"/>
+                {touched.usersname && errors.usersname && (
+                    <Perror>{errors.usersname}</Perror>
+                )}
+                <label>Email Address</label>
+                <Field type="text" name="email"className="inputForm"/>
+                {touched.email && errors.email && (
+                    <Perror>{errors.email}</Perror>
+                )}
+                <label>Password</label>
+                <Field type="password" name="password" className="inputForm"/>
+                {touched.password && errors.password && (
+                    <Perror>{errors.password}</Perror>
+                )}
                 <label>Terms of Service
                 <Field type="checkbox" name="termsservice" checked={values.termsservice} />
                 </label>
                 <TheButton> Submit </TheButton>
             </Form>
-        </div>
+        </FormDiv>
     )
 }
 
@@ -34,8 +58,14 @@ const FormikUserForm = withFormik({
             email: email || "",
             password: password || "",
             termsservice: termsservice || false
-        }
-    }
+        };
+    },
+
+    validationSchema: Yup.object().shape({
+        usersname: Yup.string().required('* Please input your name'),
+        email: Yup.string().email('* Email not valid').required('* Email is required'),
+        password: Yup.string().min(8, '* Password must be 8 characters or longer').required('* Password is required')
+    })
 })(UserForm)
 
 export default FormikUserForm
